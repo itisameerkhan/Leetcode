02ff4d41cd481d5c89c21f45177f8a2ca4521ae7
itisameerkhan <itisameerkhan@gmail.com>
2023-12-26 20:04:40 +0530

    14ms - 61.11% and 47.28mb - 11.08%

--- 0542. 01 Matrix/solution1.java
+++ 0542. 01 Matrix/solution1.java
@@ -63,7 +63,6 @@ public class solution1 {
         
         Solution sol = new Solution();
         int[][] result = sol.updateMatrix(matrix);
-        
         for(int i=0;i<result.length;i++) {
             for(int j=0;j<result[i].length;j++) {
                 System.out.print(result[i][j] + " ");
