1c66998d6be28c89fdc9c0a1d837dfa7f609390c
itisameerkhan <itisameerkhan@gmail.com>
2023-12-29 17:30:49 +0530

    0ms - 100.00% and 39.97mb - 25.07%

---
+++ 2396. Strictly Palindromic Number/solution2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int calculateBase(int n, int base) {
+        return false;
+    }
+}
+public class solution2 {
+    public static void main(String[] args) {
+        int n = 9;
+        Solution sol = new Solution();
+        if(sol.isStrictlyPalindromic(n)) System.out.println("true");
+        else System.out.println("false");
+    }
+}
