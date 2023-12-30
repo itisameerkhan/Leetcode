666e0ffaf7d7b2a5a8ea0c9ff239cbc9fd63d151
itisameerkhan <itisameerkhan@gmail.com>
2023-12-30 08:42:21 +0530

    10ms - 19.77% and 44

---
+++ 0415. Add Strings/solution1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        num1 = new StringBuilder(num1).reverse().toString();
+        num2 = new StringBuilder(num2).reverse().toString();
+        String result = "";
+        int carry = 0;
+        for (int i = 0; i < num1.length() || i < num2.length(); i ++) {
+            int c1 = 0;
+            int c2 = 0;
+            if (i < num1.length()) {
+                c1 = num1.charAt(i) - '0';
+            }
+            if (i < num2.length()) {
+                c2 = num2.charAt(i) - '0';
+            }
+            int sum = c1 + c2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+            result = digit + result;
+        }
+        if (carry != 0) {
+            result = carry + result;
+        }
+        return result;
+    }
+}
+public class solution1 {
+    public static void main(String[] args) {
+        String s1 = "11";
+        String s2 = "123";
+        Solution sol = new Solution();
+        System.out.println(sol.addStrings(s1, s2));
+    }
+}
