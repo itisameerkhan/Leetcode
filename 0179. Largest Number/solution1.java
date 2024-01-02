875eafbf65433f8b74553016db0fd7eaa3dcabcd
itisameerkhan <itisameerkhan@gmail.com>
2024-01-02 09:51:36 +0530

    13ms - 7.34% and 43.26mb - 55.47%

---
+++ 0179. Largest Number/solution1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public String largestNumber(int[] nums) {
+        List<String> stringList = new ArrayList<>();
+        for(Integer i: nums) {
+            stringList.add(i.toString());
+        }
+        Collections.sort(stringList, new Comparator<String>() {
+            public int compare(String a, String b) {
+                return (b + a).compareTo(a + b);
+            }
+        });
+        int index=0;
+        String result="";
+        while(index < stringList.size() && stringList.get(index).equals("0")) index++;
+        while(index < stringList.size()) {
+            result += stringList.get(index);
+            index++;
+        } 
+        if(result.equals("")) return "0";
+        return result;
+    }
+}
+public class solution1 {
+    public static void main(String[] args) {
+        int[] nums = {3,30,34,5,9};
+        Solution sol = new Solution();
+        System.out.println(sol.largestNumber(nums));
+    }
+}
