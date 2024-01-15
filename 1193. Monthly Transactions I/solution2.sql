75315df4eb0fcdb7d576e7d041e119e4e24016f3
itisameerkhan <itisameerkhan@gmail.com>
2024-01-15 09:37:21 +0530

    1296ms - 25.02%

---
+++ 1193. Monthly Transactions I/solution2.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Transactions (
+	id INT PRIMARY KEY,
+    country VARCHAR(255),
+    state ENUM('approved','declined'),
+    amount INT,
+    trans_date DATE
+);
+
+insert into Transactions (id, country, state, amount, trans_date) values ('121', 'US', 'approved', '1000', '2018-12-18');
+insert into Transactions (id, country, state, amount, trans_date) values ('122', 'US', 'declined', '2000', '2018-12-19');
+insert into Transactions (id, country, state, amount, trans_date) values ('123', 'US', 'approved', '2000', '2019-01-01');
+insert into Transactions (id, country, state, amount, trans_date) values ('124', 'DE', 'approved', '2000', '2019-01-07');
+
+SELECT 
+	DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    SUM(state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM transactions 
+GROUP BY 
+	DATE_FORMAT(trans_date, '%Y-%m'),
+    country;
+
+    
+
