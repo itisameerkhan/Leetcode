44ff2fc4cbc3d0a5e3b4a2a1f7be2ec32f6de255
itisameerkhan <itisameerkhan@gmail.com>
2024-01-15 13:00:32 +0530

    14ms - 56.55% and 52.86mb - 88.28%

---
+++ 0916. Word Subsets/solution1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public List<String> wordSubsets(String[] A, String[] B) {
+        int[] bmax = count("");
+        for (String b: B) {
+            int[] bCount = count(b);
+            for (int i = 0; i < 26; ++i)
+                bmax[i] = Math.max(bmax[i], bCount[i]);
+        }
+
+        List<String> ans = new ArrayList();
+        search: for (String a: A) {
+            int[] aCount = count(a);
+            for (int i = 0; i < 26; ++i)
+                if (aCount[i] < bmax[i])
+                    continue search;
+            ans.add(a);
+        }
+
+        return ans;
+    }
+
+    public int[] count(String S) {
+        int[] ans = new int[26];
+        for (char c: S.toCharArray())
+            ans[c - 'a']++;
+        return ans;
+    }
+}
+public class solution1 {
+    public static void main(String[] args) {
+        String[] words1 = {"amazon","apple","facebook","google","leetcode"};
+        String[] words2 = {"e","o"};
+        Solution sol = new Solution();
+        List<String> result = sol.wordSubsets(words1, words2);
+        for(int i=0;i<result.size();i++) System.out.print(result.get(i) + " ");
+    }
+}
